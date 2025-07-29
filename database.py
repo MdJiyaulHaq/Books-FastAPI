82339bd5ff3d7c8d88e273fc47ccb082d28a6f24
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-07-29 15:36:06 +0545

    fix database error

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://bookuser:mypassword@db:5432/bookdatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://books_database_v0ap_user:6nvk5m7SfaJYIRqrnWKORoq1lbpE4mpw@dpg-d20q0iemcj7s73e1dj80-a/books_database_v0ap"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
